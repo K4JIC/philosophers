6d22c4cfe363147df3cb737a7574869f8b3212de
Tomoya Ozaki <tozaki@c1r6s4.42tokyo.jp>
2025-12-20 20:58:12 +0900

    add dprintf

---
+++ philo/handle_input.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_input.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tozaki <tozaki@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/20 20:35:50 by tozaki            #+#    #+#             */
+/*   Updated: 2025/12/20 20:58:01 by tozaki           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	validate_input(char **argv);
+{
+}
+
+int	init_philostatus(char *)
+
+int	main(int argc, char **argv)
+{
+	if (argc != 5)
+	{
+		write(1, "Error : Wrong arguments\n", 24);
+		return (1);
+	}
+
+}
--- philo/pthread_training.c
+++ philo/pthread_training.c
@@ -6,18 +6,44 @@
 /*   By: tozaki <tozaki@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/13 17:24:40 by tozaki            #+#    #+#             */
-/*   Updated: 2025/12/13 17:39:35 by tozaki           ###   ########.fr       */
+/*   Updated: 2025/12/20 19:37:41 by tozaki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include <stdio.h>
 #include <pthread.h>
 
-void	*thread_fun
+void	*thread_func(void *param)
+{
+	int	threadnum = *(int *)(param);
+
+	printf("This is %d th thread\n", threadnum);
+	return (0);
+}
 
 int	main(void)
 {
-	pthread_t	thread;
-	int	ret = 0;
+	pthread_t	*threads;
+	int			n;
+	int			*nlst;
 
-	ret = pthread_create(&thread, NULL, thread_func, NULL);
+	n = 10;
+	nlst = (int *)malloc(sizeof(int) * n);
+	if (!nlst)
+		return (1);
+	for (int i = 0; i < n; i++)
+	{
+		nlst[i] = i;
+	}
+	threads = (pthread_t *)malloc(sizeof(pthread_t) * n);
+	if (!threads)
+		return (1);
+	for (int i = 0; i < n; i++)
+	{
+		pthread_create(&(threads[i]), NULL, thread_func, &nlst[i]);
+	}
+	for (int i = 0; i < n; i++)
+		pthread_join(threads[i], NULL);
+	return (0);
 }
